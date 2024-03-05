644254e4f9887a1aa933b26cfafea0d0e16dc545
ciph3r-dragon <152627102+ciph3r-dragon@users.noreply.github.com>
2024-03-05 17:21:46 +0100

    Algorithm for the arithmetic calculator

---
+++ script.py
@@ -0,0 +1,9 @@
+#Algorithm for the Arithmetic Calculator 
+
+#1-Start the program
+#2-Take two numbers as input using input() function
+#3-Take operation to be performed using input() function
+#4-Use conditional logic, if-else to select the operation to perform on the two operands
+#5-Store the computed value in an object named result
+#6-Print result
+#7-End program
